2517bfe92d81825aa9b1f411029318828499b44f
HfC645 <13594360004@163.com>
2026-05-30 17:11:43 +0800

    首次提交

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "C:/Users/Lenovo/Desktop/code/bin/libmain.dll.a"
+  "C:/Users/Lenovo/Desktop/code/bin/main.exe"
+  "C:/Users/Lenovo/Desktop/code/bin/main.exe.manifest"
+  "C:/Users/Lenovo/Desktop/code/bin/main.pdb"
+  "CMakeFiles/main.dir/src/main.c.obj"
+  "CMakeFiles/main.dir/src/main.c.obj.d"
+  "CMakeFiles/main.dir/src/print.c.obj"
+  "CMakeFiles/main.dir/src/print.c.obj.d"
+  "CMakeFiles/main.dir/src/print1.c.obj"
+  "CMakeFiles/main.dir/src/print1.c.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/print1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "print.h"
+
+void print1()
+{
+    printf("Hello XLab!");
+}
